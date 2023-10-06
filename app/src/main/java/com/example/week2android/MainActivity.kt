d67ee298f35d39ec4e91035d787be8b2e6fb0017
Abbas Hussain Muzammil <a.lnu25@vikes.csuohio.edu>
2023-10-06 13:35:09 -0400

    Updated with random image code

--- app/src/main/java/com/example/week2android/MainActivity.kt
+++ app/src/main/java/com/example/week2android/MainActivity.kt
@@ -24,15 +24,39 @@ class MainActivity : AppCompatActivity() {
             val newImageDrawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.image1)
             _binding.imageView.setImageDrawable(newImageDrawable)
 
-            setImageUsingGlide()
+//            setImageUsingGlide()
+
+            // When the button is clicked, select a random image URL from the list
+            val randomImageUrl = imageUrls.random()
+
+            // Load and display the randomly selected image using Glide
+            loadImageUsingGlide(randomImageUrl)
         }
     }
 
-    private fun setImageUsingGlide() {
+    private fun loadImageUsingGlide(imageUrl: String) {
         Glide
             .with(this)
-            .load("https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg")
+            .load(imageUrl)
             .centerCrop()
-            .into(_binding.imageView);
+            .into(_binding.imageView)
     }
+
+
+
+
+    private val imageUrls = listOf(
+
+//        "https://drive.google.com/uc?export=view&id=https://drive.google.com/file/d/1enWHfRhLCbEwft9YyiVcfVkM788z0VFy/view?usp=drive_link",
+//        "https://drive.google.com/uc?export=view&id=https://drive.google.com/file/d/1t-LFAiWJ4MOq9OmhPb4EeDLzvpvNAcdT/view?usp=sharing"
+//        "https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg",
+        "https://th.bing.com/th/id/OIP.cRiYwRIyOs94Hm9QyV3wbgHaHa?w=198&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th?id=OIP.8ldccR0PEFbLZh8bA6uN3wHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+
+
+
+    // Add more image URLs as needed
+        // Add more image URLs as needed
+    )
+
 }
